58f1f2078d6cd1eb1747c40566cd459e7579505e
AdityacodeNIT <srivastavaditya145@gmail.com>
2025-12-31 12:31:20 +0530

    Fix response message formatting in the root route

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, Deplotyer welcome to the ec2 instance deployment code');
+    res.send('Hello, Deplotyer welcome to the ec2 instance deployment code\n');
 });
 
 app.listen(PORT, () => {

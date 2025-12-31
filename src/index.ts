765fd963c290068c765efbaa30888c02dd9aa98f
AdityacodeNIT <srivastavaditya145@gmail.com>
2025-12-31 13:24:10 +0530

    Update root route response to include dynamic name greeting

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
-import express,{Request,Response} from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, Deplotyer welcome to the ec2 instance deployment code\n');
+// Parse form data
+app.use(express.urlencoded({ extended: true }));
+
+// Show form
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+    <html>
+      <body>
+        <form method="POST" action="/submit">
+          <label>Enter your name:</label><br/>
+          <input type="text" name="name" required />
+          <br/><br/>
+          <button type="submit">Submit</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+// Handle form submission
+app.post("/submit", (req: Request, res: Response) => {
+  const name = req.body.name || "Guest";
+  res.send(`Hello ${name}  `);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} hello from the ec2 instances`);
+  console.log(`Server is running on port ${PORT}`);
 });
